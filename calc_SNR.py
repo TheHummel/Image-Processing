2fd5080d031645279cc5620cce9e8a89fcf83e75
jannisalsbach <jannisalsbach@gmail.com>
2024-07-31 23:35:23 +0200

    use square under circle as bg reference

--- calc_SNR.py
+++ calc_SNR.py
@@ -1,6 +1,8 @@
+import os
 import numpy as np
 import rawpy
 from PIL import Image
+from tqdm import tqdm
 
 import matplotlib.pyplot as plt
 
@@ -22,36 +24,70 @@ def calc_SNR(img: np.array, center: (int, int), radius: int) -> float:
     mask = (x - center[0]) ** 2 + (y - center[1]) ** 2 <= radius**2
     circle = img[mask]
 
-    plt.imshow(mask)
-    plt.show()
+    # plt.imshow(mask)
+    # plt.show()
 
-    # TODO: use small square inside circle to calculate signal
     signal = np.mean(circle)
+    # TODO: use small square inside circle to calculate signal
 
-    bg = img[~mask]
+    diameter = 2 * radius
+    square_top_left_y = center[1] + radius + 100
+    square_top_left_x = center[0] - radius
+    square_mask = np.zeros_like(img, dtype=bool)
+    square_mask[
+        square_top_left_y : square_top_left_y + diameter,
+        square_top_left_x : square_top_left_x + diameter,
+    ] = True
+    bg_square = img[square_mask]
+
+    # plt.imshow(square_mask)
+    # plt.show()
+
     std = np.std(img)
-    std_bg = np.std(bg)
-    mean_bg = np.mean(bg)
+    std_bg = np.std(bg_square)
+    mean_bg = np.mean(bg_square)
 
-    noise = std_bg
-
-    snr = signal / noise
+    snr = signal / mean_bg
 
     return snr, signal, std, std_bg, mean_bg
 
 
-path = "images/xiaomi_max_single.dng"
-with rawpy.imread(path) as raw:
-    # Convert the raw data to a NumPy array (RGB)
-    image = raw.postprocess()
-    # image = np.rot90(image, 3)
+# calculating average metrics for single captures
+path = "C:/Users/janni/Desktop/ETH/BT/Messungen/Xiaomi/iso12800expo30_8DC_native_camera"
+output_path = "NREA/metrics/metrics_single_captures_avg.txt"
+center = (1577, 2069)
+radius = 102
 
+snr_values = []
+signal_values = []
+std_values = []
+std_bg_values = []
+mean_bg_values = []
+for filename in tqdm(os.listdir(path)):
+    if filename.endswith(".dng"):
+        with rawpy.imread(path + "/" + filename) as raw:
+            image = raw.postprocess()
+            snr, signal, std, std_bg, mean_bg = calc_SNR(image, center, radius)
+            snr_values.append(snr)
+            signal_values.append(signal)
+            std_values.append(std)
+            std_bg_values.append(std_bg)
+            mean_bg_values.append(mean_bg)
 
-# path = "image_stacking/xiaomi_max_mean.tiff"
+SNR = np.mean(snr_values)
+signal = np.mean(signal_values)
+std = np.mean(std_values)
+std_bg = np.mean(std_bg_values)
+mean_bg = np.mean(mean_bg_values)
+
+# calculating metrics for NREA output
+# setting = "ca_r5"
+# output_path = "NREA/metrics/metrics_" + setting + ".txt"
+# path = "NREA/images/nrea_with_" + setting + ".tiff"
 # image = Image.open(path)
 # image = np.array(image)
 
-SNR, signal, std, std_bg, mean_bg = calc_SNR(image, (1577, 2069), 102)
+# SNR, signal, std, std_bg, mean_bg = calc_SNR(image, (1577, 2069), 102)
 
 print(f"SNR: {SNR}")
 print(f"Signal: {signal}")
@@ -60,7 +96,7 @@ print(f"Standard Deviation (Background): {std_bg}")
 print(f"Mean (Background): {mean_bg}")
 
 # save results to txt file
-with open("SNR_results.txt", "w") as f:
+with open(output_path, "w") as f:
     f.write(f"SNR: {SNR}\n")
     f.write(f"Signal: {signal}\n")
     f.write(f"Standard Deviation: {std}\n")
