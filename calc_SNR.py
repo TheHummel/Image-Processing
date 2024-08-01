654b858c679e97347360988ea5a12645bc678bed
jannisalsbach <jannisalsbach@gmail.com>
2024-08-01 16:37:30 +0200

    refactor calc snr

--- calc_SNR.py
+++ calc_SNR.py
@@ -53,13 +53,67 @@ def calc_SNR(img: np.array, center: Tuple[int, int], radius: int) -> float:
     return snr, signal, std, std_bg, mean_bg
 
 
-# calculating average metrics for a folder of single captures
-path = "C:/Users/janni/Desktop/ETH/BT/Messungen/Xiaomi/iso12800expo30_8DC_native_camera"
-output_path = "NREA/metrics/metrics_single_captures_avg.txt"
-center = (1577, 2069)
-radius = 102
+def save_metrics(
+    SNR: float,
+    signal: float,
+    std: float,
+    std_bg: float,
+    mean_bg: float,
+    output_path: str,
+) -> None:
+    print(f"SNR: {SNR}")
+    print(f"Signal: {signal}")
+    print(f"Standard Deviation: {std}")
+    print(f"Standard Deviation (Background): {std_bg}")
+    print(f"Mean (Background): {mean_bg}")
 
-snr_values = []
+    # save results to txt file
+    with open(output_path, "w") as f:
+        f.write(f"SNR: {SNR}\n")
+        f.write(f"Signal: {signal}\n")
+        f.write(f"Standard Deviation: {std}\n")
+        f.write(f"Standard Deviation (Background): {std_bg}\n")
+        f.write(f"Mean (Background): {mean_bg}\n")
+
+
+# calculating AVERAGE metrics for a folder of single captures
+# path = "C:/Users/janni/Desktop/ETH/BT/Messungen/Xiaomi/iso12800expo30_8DC_native_camera"
+# output_path = "NREA/metrics/metrics_single_captures_avg.txt"
+# center = (1577, 2069)
+# radius = 102
+
+# snr_values = []
+# signal_values = []
+# std_values = []
+# std_bg_values = []
+# mean_bg_values = []
+# for filename in tqdm(os.listdir(path)):
+#     if filename.endswith(".dng"):
+#         with rawpy.imread(path + "/" + filename) as raw:
+#             image = raw.postprocess()
+#             snr, signal, std, std_bg, mean_bg = calc_SNR(image, center, radius)
+#             snr_values.append(snr)
+#             signal_values.append(signal)
+#             std_values.append(std)
+#             std_bg_values.append(std_bg)
+#             mean_bg_values.append(mean_bg)
+
+# SNR = np.mean(snr_values)
+# signal = np.mean(signal_values)
+# std = np.mean(std_values)
+# std_bg = np.mean(std_bg_values)
+# mean_bg = np.mean(mean_bg_values)
+
+# save_metrics(SNR, signal, std, std_bg, mean_bg, output_path)
+
+# calculating metrics for a folder of single captures
+path = "C:/Users/janni/Desktop/ETH/BT/Messungen/compare_native_to_custom/Huawei P20 Pro/custom/iso3200expo30"
+output_path = "C:/Users/janni/Desktop/ETH/BT/Messungen/compare_native_to_custom/Huawei P20 Pro/custom_metrics/iso3200expo30"
+
+center = (1442, 1975)
+radius = 114
+
+SNR_values = []
 signal_values = []
 std_values = []
 std_bg_values = []
@@ -68,18 +122,38 @@ for filename in tqdm(os.listdir(path)):
     if filename.endswith(".dng"):
         with rawpy.imread(path + "/" + filename) as raw:
             image = raw.postprocess()
+            image = np.rot90(image, 3)
             snr, signal, std, std_bg, mean_bg = calc_SNR(image, center, radius)
-            snr_values.append(snr)
+            save_metrics(
+                snr, signal, std, std_bg, mean_bg, output_path + "/" + filename + ".txt"
+            )
+
+            SNR_values.append(snr)
             signal_values.append(signal)
             std_values.append(std)
             std_bg_values.append(std_bg)
             mean_bg_values.append(mean_bg)
 
-SNR = np.mean(snr_values)
+SNR = np.mean(SNR_values)
+SNR_std = np.std(SNR_values)
 signal = np.mean(signal_values)
+signal_std = np.std(signal_values)
 std = np.mean(std_values)
+std_std = np.std(std_values)
 std_bg = np.mean(std_bg_values)
+std_bg_std = np.std(std_bg_values)
 mean_bg = np.mean(mean_bg_values)
+mean_bg_std = np.std(mean_bg_values)
+
+os.makedirs(output_path, exist_ok=True)
+
+with open(output_path + "/metrics_summary.txt", "w") as f:
+    f.write(f"SNR: {SNR} +/- {SNR_std}\n")
+    f.write(f"Signal: {signal} +/- {signal_std}\n")
+    f.write(f"Standard Deviation: {std} +/- {std_std}\n")
+    f.write(f"Standard Deviation (Background): {std_bg} +/- {std_bg_std}\n")
+    f.write(f"Mean (Background): {mean_bg} +/- {mean_bg_std}\n")
+
 
 # calculating metrics single capture
 # setting = "ca_r5"
@@ -90,16 +164,4 @@ mean_bg = np.mean(mean_bg_values)
 
 # SNR, signal, std, std_bg, mean_bg = calc_SNR(image, (1577, 2069), 102)
 
-print(f"SNR: {SNR}")
-print(f"Signal: {signal}")
-print(f"Standard Deviation: {std}")
-print(f"Standard Deviation (Background): {std_bg}")
-print(f"Mean (Background): {mean_bg}")
-
-# save results to txt file
-with open(output_path, "w") as f:
-    f.write(f"SNR: {SNR}\n")
-    f.write(f"Signal: {signal}\n")
-    f.write(f"Standard Deviation: {std}\n")
-    f.write(f"Standard Deviation (Background): {std_bg}\n")
-    f.write(f"Mean (Background): {mean_bg}\n")
+# save_metrics(SNR, signal, std, std_bg, mean_bg, output_path)
