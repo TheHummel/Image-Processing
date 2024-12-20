1bf78c9b46f998a1c38b5cf15b8aed6716002fea
jannisalsbach <jannisalsbach@gmail.com>
2024-12-20 10:27:03 +0100

    script to calc metrics for folder of imgs

---
+++ calc_metrics_for_folder.py
@@ -0,0 +1,46 @@
+import os
+import click
+import pandas as pd
+from tqdm import tqdm
+
+from metrics.SNR_metrics import calc_SNR
+from helpers.helpers import load_images_from_folder, load_dngs_from_folder
+from helpers.CLI_options import (
+    input_dir_option,
+    is_raw_option,
+    center_x_option,
+    center_y_option,
+    radius_option,
+)
+
+
+@click.command()
+@input_dir_option
+@is_raw_option
+@center_x_option
+@center_y_option
+@radius_option
+def calc_metrics_reliability(
+    input_dir: str, is_raw: bool, center_x: int, center_y: int, radius: int
+):
+    # LOAD IMAGES
+    if is_raw:
+        images, _ = load_dngs_from_folder(input_dir)
+    else:
+        images, _ = load_images_from_folder(input_dir)
+
+    center = (center_x, center_y)
+
+    metrics = []
+    for image in tqdm(images, desc="Calculating metrics", total=len(images)):
+        snr, signal, noise = calc_SNR(image, center, radius, show_sample_position=False)
+        metrics.append([snr, signal, noise])
+
+    # save metrics to csv
+    df = pd.DataFrame(metrics, columns=["SNR", "Signal", "Noise"])
+
+    df.to_csv(input_dir + "/all_metrics.csv")
+
+
+if __name__ == "__main__":
+    calc_metrics_reliability()
