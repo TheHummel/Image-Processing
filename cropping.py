b525b91dd3102291f3b04a471704e5c2ee09261b
jannisalsbach <jannisalsbach@gmail.com>
2025-02-14 17:35:55 +0100

    update cropping to 16bit

--- cropping.py
+++ cropping.py
@@ -2,6 +2,7 @@ import os
 from PIL import Image
 from tqdm import tqdm
 import click
+import numpy as np
 
 from helpers.helpers import load_images_from_folder, load_dngs_from_folder_16bit
 from helpers.CLI_options import input_dir_option, is_raw_option, crop_factor_option
@@ -22,13 +23,20 @@ def crop(input_dir: str, is_raw: bool, crop_factor: int):
         images, _ = load_images_from_folder(input_dir)
 
     for i, image in tqdm(enumerate(images), desc="Cropping images", total=len(images)):
+        if image.dtype != np.uint16:
+            image = image.astype(np.uint16)
         square_size = int(min(image.shape[0], image.shape[1]) / crop_factor)
         start_x = image.shape[1] // 2 - square_size // 2
         start_y = image.shape[0] // 2 - square_size // 2
         image = image[start_y : start_y + square_size, start_x : start_x + square_size]
-        im = Image.fromarray(image)
+        if is_raw:
+            im = Image.fromarray(
+                image[:, :, 2].astype(np.uint16), mode="I;16"
+            )  # only uses blue channel
+        else:
+            im = Image.fromarray(image, mode="I;16")
 
-        im.save(output_path + f"/cropped_{i + 1}.tiff")
+        im.save(output_path + f"/cropped_{i + 1}.tiff", format="TIFF")
 
 
 if __name__ == "__main__":
